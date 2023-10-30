046a42910d4d88d3c3bba74ac92284126b0923bf
Alejandro maiolo <84913211+Acm2021@users.noreply.github.com>
2023-10-30 15:07:56 -0300

    Add files via upload

---
+++ EntregaFinalJs-Maiolo/scripts/main.js
@@ -0,0 +1,152 @@
+
+
+/*---------------------------------------------------------*/
+function agregarProductoAlCarrito(evt,galeria,carrito){
+    if(evt.target.classList.contains("AgregarAlCarrito")){
+        const producto =evt.target.parentElement
+        const id =producto.querySelector("#card-id").textContent
+        let productoEncontrado = galeria.buscarProductoPorId(id)
+        carrito.agregarUnProducto(productoEncontrado)
+        carrito.mostrarPorPantalla()  
+    }
+}
+function eliminarProductoDelCarrito(evt,carrito){
+    
+    if(evt.target.id==="borrarProducto"){
+        idProductoABorrar =evt.target.getAttribute('data-id')
+        carrito.eliminarUnProducto(idProductoABorrar)
+        carrito.mostrarPorPantalla();
+    }
+}
+function vaciarCarrito(evt,carrito){
+    carrito.vaciar()
+    carrito.mostrarPorPantalla()
+}
+function filtrarGaleria(evt,galeria){
+    const nombreAFiltrar=document.querySelector("#busquedaNombre")
+    const marcaAFiltrar=document.querySelector("#busquedaMarca")
+    const tipoAFiltrar=document.querySelector("#busquedaTipo")
+    const galeriaFiltrada = new GaleriaProductos(galeria.filtrarProductos(nombreAFiltrar.value,marcaAFiltrar.value,tipoAFiltrar.value));
+    galeriaFiltrada.mostrarGaleriaPorPantalla()
+}
+function borrarFiltroCarrito(evt,galeria){
+    const nombreAFiltrar=document.querySelector("#busquedaNombre")
+    const marcaAFiltrar=document.querySelector("#busquedaMarca")
+    const tipoAFiltrar=document.querySelector("#busquedaTipo")
+    nombreAFiltrar.value= null
+    marcaAFiltrar.value=null
+    tipoAFiltrar.value=null
+    galeria.mostrarGaleriaPorPantalla();
+}
+function actualizarLocalStorage(elementToSave){
+    localStorage.setItem('carrito', JSON.stringify(elementToSave))
+}
+
+
+
+async function dataRequest (path){
+    try{
+        const response = await fetch(path);
+        if (!response.ok) {
+            throw new Error('Error al cargar el archivo JSON');
+        }
+        const jsonData = await response.json();
+        return jsonData
+        
+    }catch(error){
+        Swal.fire(error)
+    };
+}
+
+function jsonDataParseProducts(jsonData){
+    
+    if (jsonData && jsonData.productos) {
+        const arregloDeProductos = jsonData.productos.map(productoData => {
+            return new Producto(
+                productoData.nombre,
+                productoData.Tipo,
+                productoData.marca,
+                productoData.precio,
+                productoData.id
+            );
+        });
+    return arregloDeProductos
+    }
+}
+
+
+async function fetchDataAndParse(dataPath) {
+    try {
+        const jsonData = await dataRequest(dataPath); // Espera a que dataRequest se complete
+        const arregloDeProductos = jsonDataParseProducts(jsonData);
+        return arregloDeProductos;
+    } catch (error) {
+        console.error('Error al cargar y parsear los datos:', error);
+    }
+}
+
+function carritoParse(carritoDataJson){
+    if (carritoDataJson) {
+        let carritoSaved= new Carrito
+        carritoSaved= JSON.parse(carritoDataJson)
+        
+        return carritoSaved
+    }
+}
+
+function cargarCarritoConStorage(){
+    const carritoDataJson = localStorage.getItem('carrito');
+    if (carritoDataJson) {
+        carritoSaved= new Carrito()
+        carritoSaved2= new Carrito()
+        carritoSaved=carritoParse(carritoDataJson)
+        for (let i = 0; i < carritoSaved.items.length; i++) {
+            const item= new Producto()
+             item.nombre = carritoSaved.items[i].nombre;
+             item.tipo = carritoSaved.items[i].tipo;
+             item.marca = carritoSaved.items[i].marca;
+             item.precio = carritoSaved.items[i].precio;
+             item.id = carritoSaved.items[i].id;
+            carritoSaved2.items.push(item)
+        }
+        carritoSaved2.precioTotal=carritoSaved.precioTotal
+        carritoSaved2.cantidadElemento=carritoSaved.cantidadElemento
+        carritoSaved2.mostrarPorPantalla()
+        return carritoSaved2
+    } else {
+        console.log('No se encontraron datos del carrito en localStorage.');
+    }
+}
+
+//INICIO
+
+async function inicio(){
+    
+    let DATA_PATH = 'data/data.json';
+    const arregloDeProductos = await  fetchDataAndParse(DATA_PATH);
+    const galeria = new GaleriaProductos(arregloDeProductos);
+    let carrito = new Carrito();
+    
+    carrito=cargarCarritoConStorage()
+    galeria.mostrarGaleriaPorPantalla();
+
+    const contendorProductosPagina= document.querySelector("#contendorProductos")
+    contendorProductosPagina.addEventListener('click', (evt) => {agregarProductoAlCarrito(evt, galeria,carrito)})
+
+    const contendorCarrito = document.querySelector("#cuerpoFilasCarrito")
+    contendorCarrito.addEventListener('click', (evt) => {eliminarProductoDelCarrito(evt,carrito)})
+
+    const vaciarCarritoElement = document.querySelector("#vaciar-carrito")
+    vaciarCarritoElement.addEventListener('click', (evt) => {vaciarCarrito(evt,carrito)})
+
+
+    const btnFiltrarGaleria = document.querySelector("#btnFiltroGaleria")
+    btnFiltrarGaleria.addEventListener('click',(evt)=> {filtrarGaleria(evt,galeria)})
+    const btnBorrarFiltradoGaleria= document.querySelector("#btnBorrarFiltroGaleria")
+    btnBorrarFiltradoGaleria.addEventListener('click', (evt)=>{borrarFiltroCarrito(evt,galeria)})
+
+}
+
+
+inicio();
+
---
+++ scripts/clases.js
@@ -0,0 +1,217 @@
+class Producto{
+    constructor(nombre,tipo,marca,precio,id){
+    this.nombre=nombre;
+    this.tipo=tipo;
+    this.marca=marca;
+    this.precio=precio ;   
+    this.id=id;
+    }
+    mostrar(){
+        console.log('Nombre del producto:' + this.nombre)
+        console.log('Tipo:'+ this.tipo)
+        console.log('Marca:' + this.marca)
+        console.log('Precio:' + this.precio)
+        console.log('Id:' + this.id)
+    }
+    igualNombre(nombreConsultado){
+        if(this.nombre===nombreConsultado)
+            return true;
+        else 
+            return false;
+    }
+    igualTipo(tipoConsultado){
+        if(this.tipo===tipoConsultado)
+            return true;
+        else 
+            return false;
+    }
+    igualMarca(marcaConsultada){
+        if(this.marca===marcaConsultada)
+            return true;
+        else 
+            return false;
+    }
+    igualPrecio(precioConsultado){
+        if(this.precio===precioConsultado)
+            return this;
+        else 
+            return false;
+    }
+    igualId(idConsultado){
+        if(this.id===idConsultado)
+            return this;
+        else 
+            return false;
+    }
+    getNombre(){
+        return this.nombre;
+    };
+    getTipo(){
+        return this.tipo;
+    };
+    getMarca(){
+        return this.marca;
+    };
+    getPrecio(){
+        return this.precio;
+    };
+    getId(){
+        return this.id;
+    };
+    cargarElPrductoEnUnaCard(){
+        const card = document.createElement('div');
+        card.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="../img/productos/${this.id}.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${this.nombre}</h5>
+                    <p class="card-marca">${this.marca}</p>
+                    <p class="tipo-marca">${this.tipo}</p>
+                    <p id="card-id">${this.id}</p>
+                    <p class="card-precio">${this.precio}</p>
+                    <a href="#" class="btn btn-primary AgregarAlCarrito">Agregar al carrito</a>
+                </div>
+            </div>
+            `;
+        return card
+    }; 
+
+}
+
+//-----------------------------------------------------------------------------//
+
+class GaleriaProductos{
+    constructor(arrayProductos){
+           this.productos=arrayProductos;
+    }
+
+    filtrarNombre = (producto, nombreConsultado) => {
+        if (producto.igualNombre(nombreConsultado))
+            return producto }
+    filtrarMarca = (producto, marcaConsultada) => {
+        if (producto.igualMarca(marcaConsultada))
+            return producto}
+    filtrarTipo = (producto, tipoConsultado) => {
+        if (producto.igualTipo(tipoConsultado))
+            return producto}
+    filtrarPrecios = (producto, precioConsultado) => {
+        if (producto.igualPrecio(precioConsultado))
+        return producto} 
+    filtrarId = (producto, idConsultado) => {
+            if (producto.igualId(idConsultado))
+            return producto}    
+    filtrarProductos(nombreConsultado,marcaConsultada,tipoConsultado){
+        let resultadoFiltro = this.productos.filter(producto => {
+            return (
+                (nombreConsultado === "" || this.filtrarNombre(producto, nombreConsultado)) &&
+                (marcaConsultada === "" || this.filtrarMarca(producto, marcaConsultada)) &&
+                (tipoConsultado === "" || this.filtrarTipo(producto, tipoConsultado))
+            );
+        });
+        if (resultadoFiltro.length > 0) {
+            return resultadoFiltro;
+        } else {
+            Swal.fire("No hay resultados para su búqueda")
+            
+        }
+    }
+    buscarProductoPorId(idConsultado){
+        let productoEncontrado=this.productos.find((producto)=>producto.igualId(idConsultado))
+        if (productoEncontrado) {
+            return productoEncontrado;
+          } else {
+            console.log("Producto no encontrado");
+          }
+        }
+    mostrarGaleriaPorConsola(){        
+        for (const producto of this.productos) {
+                producto.mostrar();
+        }
+    };
+    limpiarPantalla(){
+        const fila= document.querySelector('#fila');  
+        while(fila.firstChild){
+            fila.removeChild(fila.firstChild)
+        }
+    }
+    mostrarGaleriaPorPantalla(){
+        this.limpiarPantalla();
+        const fila= document.querySelector('#fila');
+        for (const producto of this.productos) {
+            const colum = document.createElement('div');
+            colum.innerHTML = `<div class="col"></div>`
+            const card = producto.cargarElPrductoEnUnaCard()
+            colum.appendChild(card);
+            fila.appendChild(colum)
+        };
+    }
+}
+//-----------------------------------------------------------------------------//
+class Carrito{
+    constructor(){
+        this.items = [];
+        this.precioTotal=0;
+        this.cantidadElemento=0;
+    }
+    
+    agregarUnProducto(producto){
+            this.items.push(producto)
+            this.precioTotal+= producto.getPrecio();
+            this.precioTotal = Math.floor(this.precioTotal* 100) / 100
+            actualizarLocalStorage(this)
+   };
+    eliminarUnProducto(idAEliminar){
+       const indiceAEliminar=this.items.findIndex((item)=>item.igualId(idAEliminar))
+       if (indiceAEliminar !== -1) {
+        this.precioTotal-=this.items[indiceAEliminar].getPrecio()
+        this.precioTotal = Math.floor(this.precioTotal * 100) / 100
+        this.items.splice(indiceAEliminar, 1);
+        actualizarLocalStorage(this)
+      } else {
+        console.log("No se encontró ningún elemento con el ID especificado.");
+      }
+      
+    };
+    mostrarProductosCarritoPorConsola(){
+        for (const item of this.items) {
+            item.mostrar();
+        }
+    };
+    limpiarPantalla(){
+        const cuerpoFilasCarrito= document.querySelector('#cuerpoFilasCarrito');  
+        while(cuerpoFilasCarrito.firstChild){
+            cuerpoFilasCarrito.removeChild(cuerpoFilasCarrito.firstChild)
+        }
+    }
+    mostrarPorPantalla(){
+        this.limpiarPantalla()
+        for (const item of this.items) {
+            const cuerpoFilasCarrito=document.querySelector('#cuerpoFilasCarrito')
+            const filaCarrito= document.createElement('tr')
+            filaCarrito.innerHTML = `
+                <td><img src='../img/productos/${item.getId()}.jpg' width="100" /></td>
+                <td> ${item.getNombre()}</td>
+                <td> ${item.getPrecio()}</td>
+                <td> ${item.getId()}</td>
+                <td></td>
+                <td>  <a href='#' id= "borrarProducto" data-id="${item.getId()}">❌</a></td>
+            `
+            cuerpoFilasCarrito.appendChild(filaCarrito)
+        }
+    const totalCarrito=document.querySelector('#totalCarrito')
+    totalCarrito.textContent = "Total del carrito: $" + this.precioTotal;
+    //actualizarLocalStorage(this)
+    }
+    mostrarPrecioTotal(){
+        console.log("El precio total de carrito es:" + "$" + this.precioTotal)
+    }; 
+    vaciar(){
+        while(this.items.length>0){
+           this.items.pop()
+        }  
+        this.precioTotal=0
+        actualizarLocalStorage(this)
+    }
+}
+
+
